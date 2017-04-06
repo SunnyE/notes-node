f1cd4ad95625d2f5572bd1ca4c672b954d61f7df
Ethan Favia <efavia215@live.com>
2017-04-06 15:12:21 -0400

    code clean up

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
 
-var res = notes.addNote();
-console.log(res); 
+const notes = require('./notes.js');
 
-var add = notes.add(2,5);
 
-console.log(add);
-
-// var user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `hello ${user.username}! your are ${notes.age}`);
