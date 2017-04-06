43ca89029e6b70f38ef669e52e929de5eda7779b
Ethan Favia <efavia215@live.com>
2017-04-06 13:24:07 -0400

    more messing around with node

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res); 
 
+var add = notes.add(2,5);
 
+console.log(add);
 
-fs.appendFileSync('greetings.txt', `hello ${user.username}!`);
+// var user = os.userInfo();
+
+// fs.appendFileSync('greetings.txt', `hello ${user.username}! your are ${notes.age}`);
