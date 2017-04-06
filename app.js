75a6dc0c5d683a25527fb638b9b901cbc701a329
Ethan Favia <efavia215@live.com>
2017-04-06 16:02:22 -0400

    setting up comands

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+command = process.argv[2];
 
+console.log('Command: ', command); 
+console.log(process.argv)
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read'){
+    console.log('Reading notes')
+} else if (commmand === 'remove'){
+    console.log(console.log('Removing notes'));
+} else {
+    console.log('Command not found'); 
+}
