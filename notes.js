9ce86b7411e1e95bce1f9b47deed85506c03daee
Ethan Favia <efavia215@live.com>
2017-04-10 15:08:45 -0400

    slight code refactoring

--- notes.js
+++ notes.js
@@ -2,32 +2,37 @@ console.log('starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+    try {
+        // read the notes from data so new note can be appended before adding new
+        var notesString = fs.readFileSync('notes-data.json');
+
+        // returning the parsed json string
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    // array of notes
-    var notes = [];
+    var notes = fetchNotes();
     
     var note = {
         title,
         body
     };
-
-    try {
-        // read the notes from data so new note can be appended before adding new
-        var notesString = fs.readFileSync('notes-data.json');
-
-        // parse the json string
-        var notes = JSON.parse(notesString);
-    } catch (e) {
-
-    }
     
     var duplicateNotes = notes.filter((note) => note.title === title);
 
-    if (duplicateNotes.length > 0) { 
+    if (duplicateNotes.length === 0) { 
         // adding note to the array
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-
+        
+        saveNotes(notes);
     }
     
 };
