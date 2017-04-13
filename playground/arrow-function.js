2174f56db09c2c17bbb15c9dc514170e04c1522d
Ethan Favia <efavia215@live.com>
2017-04-13 11:20:21 -0400

    a few small things

--- playground/arrow-function.js
+++ playground/arrow-function.js
@@ -9,4 +9,23 @@ var squareArrow = x => x * x;
 
 console.log(square(9));
 
+// arrow functions do not bind a this keyword. 
+
+
+
+var uer = { 
+    name: 'Ethan',
+    sayHi: () => {
+        console.log(arguments.)
+        // this will not work because the arrow function as no reference to this
+        console.log(`hit, I'm ${this.name}`);
+    },
+    // new ES6 object method syntax
+    sayHiAlt () {
+        console.log(aryguments);
+        console.log(`Hi, I'm ${this.name}`);
+    }
+};
+
+user.sayHi();
 
